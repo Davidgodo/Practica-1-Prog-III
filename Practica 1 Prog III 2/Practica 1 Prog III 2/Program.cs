19da65ea100f66aefe972b8aef7cf2cb49d6305e
Davidgodo <90344244+Davidgodo@users.noreply.github.com>
2022-02-11 12:31:43 -0400

    Add files via upload

---
+++ Practica 1 Prog III 2/Practica 1 Prog III 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Practica_1_Prog_III_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int n = 10;
+            int[] v = new int[10];
+
+            Console.WriteLine("Vector");
+
+            for (int k = n - 1; k >= 0; k--)
+            {
+                v[k] = r.Next(0, 11);
+                Console.Write(v[k] + " ");
+            }
+
+            Console.WriteLine("Vector ordenado");
+            
+            int c;
+
+            for (int i = n - 1; i > 0; i--)
+                for (int j = 0; j < i; j++)
+                    if (v[j] > v[j + 1])
+                    {
+                        c = v[j + 1];
+                        v[j + 1] = v[j];
+                        v[j] = c;
+                    }
+
+            for (int k = n - 1; k >= 0; k--)
+            {
+                Console.Write(v[k] + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practica 1 prog III/Practica 1 prog III/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Practica_1_prog_III
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombre = new string[4];
+            int[] edad = new int[4];
+
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Escriba un nombre");
+                nombre[i] = Console.ReadLine();
+                Console.WriteLine("Escriba una edad");
+                edad[i] = int.Parse(Console.ReadLine());
+            }
+            
+            Console.WriteLine("Menu");
+            Console.WriteLine("1. Mostrar el menor");
+            Console.WriteLine("2. Ordenar por nombre");
+            Console.WriteLine("3. Ordenar por edad");
+            int r = int.Parse(Console.ReadLine());
+
+            if (r == 1)
+            {
+                //menor();
+            }
+            else if (r == 2)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            Console.ReadKey();
+        }
+
+        public void menor(int[] edad)
+        {
+            int m = edad[0];
+
+            
+        }
+    }
+}
